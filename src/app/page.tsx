ae13d6896d733376ab18f10800a07abd26d794a2
Oluwaseyipd <oluwaseyipd@gmail.com>
2026-09-14 09:27:10 +0100

    feat: update layout and page components with new favicon and styling adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-          <h1 className="text-5xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+    <div className="flex min-h-screen items-center justify-center font-sans bg-black">
+          <h1 className="text-5xl font-semibold leading-10 tracking-tight text-white">
             You're welcome to Amfida
           </h1>
-      </main>
     </div>
   );
 }
